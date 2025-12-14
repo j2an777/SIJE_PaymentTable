d59fa92e2f0c635a854e509b7c8b3ab50bc4e6a6
j2an777 <ha91104@gmail.com>
2025-12-14 15:32:04 +0900

    fix: msw development환경에서 실행하기 위한 설정 및 QueryClientProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,26 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+import makeQueryClient from './utils/makeQueryClient';
+import App from './App';
 
 async function enableMocking() {
-  if (import.meta.env.MODE === "development") {
-    const { worker } = await import("./mocks/browser");
+  if (import.meta.env.MODE === 'development') {
+    const { worker } = await import('./mocks/browser');
     await worker.start();
   }
 }
 
 enableMocking().then(() => {
-  const root = createRoot(document.getElementById("root")!);
+  const root = createRoot(document.getElementById('root')!);
+  const queryClient = makeQueryClient();
 
   root.render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   );
 });
