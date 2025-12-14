a832c56d9299ece1a95554683ee803e96cebd5ae
j2an777 <ha91104@gmail.com>
2025-12-14 18:32:01 +0900

    feat: 상위 Main에 global style 적용합니다.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
+import { Global } from '@emotion/react';
 import React from 'react';
 
 import makeQueryClient from './utils/makeQueryClient';
+import globalStyle from './styles/globalStyle';
 import App from './App';
 
 async function enableMocking() {
@@ -19,6 +21,7 @@ enableMocking().then(() => {
   root.render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
+        <Global styles={globalStyle} />
         <App />
       </QueryClientProvider>
     </React.StrictMode>
