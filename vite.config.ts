7b6cb96ba780e3537b146e1f5342fd06e35c1ae2
j2an777 <ha91104@gmail.com>
2025-12-14 18:21:58 +0900

    chore: alias 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/styles': path.resolve(__dirname, 'src/styles'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/apis': path.resolve(__dirname, 'src/apis'),
+      '@/types': path.resolve(__dirname, 'src/types'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+      '@/assets': path.resolve(__dirname, 'src/assets'),
+    },
+  },
+});
